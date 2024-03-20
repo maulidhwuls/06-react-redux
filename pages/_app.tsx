ed882c8658c167d9a4f7780a1ea5c389bf029c51
maulidhwuls <maulidiahwulan@gmail.com>
2024-03-20 15:28:56 +0800

    W06 - Praktikum 1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />;
+// }
+
+function MyApp({ Component, pageProps}){
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
